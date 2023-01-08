f7a8d47d779a6a85f498dd67699a6b5bc59e0821
YangX29 <1214719907@qq.com>
2023-01-08 21:23:36 +0800

    Update LogUtils.kt
    添加json日志输出方法

--- module_common/src/main/java/com/example/module_common/log/LogUtils.kt
+++ module_common/src/main/java/com/example/module_common/log/LogUtils.kt
@@ -1,4 +1,8 @@
-package com.example.module_common.utils.log
+package com.example.module_common.log
+
+import com.example.module_common.utils.log.MLog
+import org.json.JSONArray
+import org.json.JSONObject
 
 /**
  * @author: Yang
@@ -7,13 +11,30 @@ package com.example.module_common.utils.log
  */
 object LogUtils {
 
+    private const val INDENT_SPACES = 4
+
     /**
      * 打印json数据
      */
-    fun printJson(tag: String? = null, json: String) {
-        // TODO json字符串格式化
-        //打印日志
-        MLog.d(tag, json)
+    fun printJson(tag: String, json: String) {
+        val result = StringBuilder()
+        // json字符串格式化
+        if (json.startsWith("{")) {
+            val jsonObject = JSONObject(json)
+            result.append(jsonObject.toString(INDENT_SPACES))
+        } else if (json.startsWith("[")) {
+            val jsonArray = JSONArray(json)
+            result.append(jsonArray.toString(INDENT_SPACES))
+        } else {
+            result.append(json)
+        }
+        // 分行
+        val separator = System.getProperty("line.separator") ?: ""
+        val list = result.split(separator)
+        // 输出日志
+        list.forEach {
+            MLog.d(tag, it)
+        }
     }
 
 }
