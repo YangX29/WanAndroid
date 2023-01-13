729ef1890d9aa5b8a096c929cdb3f0e1c0de344d
oyyj_rl <1214719907@qq.com>
2023-01-13 17:42:58 +0800

    屏幕工具类ScreenUtil修改，添加新的方法

--- module_common/src/main/java/com/example/module_common/utils/screen/ScreenUtil.kt
+++ module_common/src/main/java/com/example/module_common/utils/screen/ScreenUtil.kt
@@ -11,64 +11,65 @@ import android.os.Build
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.math.abs
 
 
 /**
  * @author: Yang
  * @date: 2023/1/12
  * @description: 屏幕工具类
+ * TODO 使用[WindowInsets]的一系列方法有待验证
  */
 object ScreenUtil {
 
     /**
-     * 获取屏幕宽度
-     * TODO 是否包含状态栏和导航栏高度
+     * 获取屏幕宽度，不包含状态栏和导航栏高度
      */
     fun getScreenWidth(): Int {
         return Resources.getSystem().displayMetrics.widthPixels
     }
 
     /**
-     * 获取屏幕高度
-     * TODO 是否包含状态栏和导航栏高度
+     * 获取屏幕高度，不包含状态栏和导航栏高度
      */
     fun getScreenHeight(): Int {
         return Resources.getSystem().displayMetrics.heightPixels
     }
 
     /**
-     * 获取屏幕宽高比
+     * 获取屏幕宽高比，不包含状态栏和导航栏高度
      */
     fun getScreenRatio(): Float {
         return (getScreenWidth() / getScreenHeight()).toFloat()
     }
 
     /**
-     * 获取屏幕宽度
+     * 获取真实屏幕宽度
      */
-    fun getScreenWidth(context: Context): Int {
-        return getScreenSize(context)?.x ?: 0
+    fun getScreenRealWidth(context: Context): Int {
+        return getScreenRealSize(context)?.x ?: 0
     }
 
     /**
-     * 获取屏幕高度，不包含状态栏
+     * 获取真实屏幕高度
      */
-    fun getScreenHeight(context: Context): Int {
-        return getScreenSize(context)?.y ?: 0
+    fun getScreenRealHeight(context: Context): Int {
+        return getScreenRealSize(context)?.y ?: 0
     }
 
     /**
      * 获取屏幕宽高比
      */
-    fun getScreenRatio(context: Context): Float {
-        return (getScreenWidth(context) / getScreenHeight(context)).toFloat()
+    fun getScreenRealRatio(context: Context): Float {
+        return (getScreenRealWidth(context) / getScreenRealHeight(context)).toFloat()
     }
 
     /**
-     * 获取屏幕大小
-     * TODO 是否包含状态栏和导航栏高度
+     * 获取屏幕真实大小，包含状态栏和导航栏高度
      */
-    fun getScreenSize(context: Context): Point? {
+    fun getScreenRealSize(context: Context): Point? {
         val wm = ContextCompat.getSystemService(context, WindowManager::class.java) ?: return null
         val point = Point()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -83,8 +84,29 @@ object ScreenUtil {
     }
 
     /**
+     * 通过insets获取状态栏高度，需要window添加后才能拿到，可以在[View.post]方法中获取
+     */
+    fun getStatusBarHeightByInsets(activity: Activity): Int {
+        val top = ViewCompat.getRootWindowInsets(activity.window.decorView)
+            ?.getInsetsIgnoringVisibility(WindowInsetsCompat.Type.statusBars())?.top ?: 0
+        val bottom = ViewCompat.getRootWindowInsets(activity.window.decorView)
+            ?.getInsetsIgnoringVisibility(WindowInsetsCompat.Type.statusBars())?.bottom ?: 0
+        return abs(top - bottom)
+    }
+
+    /**
+     * 通过insets获取导航栏高度，需要window添加后才能拿到，可以在[View.post]方法中获取
+     */
+    fun getNavBarHeightByInsets(activity: Activity): Int {
+        val top = ViewCompat.getRootWindowInsets(activity.window.decorView)
+            ?.getInsetsIgnoringVisibility(WindowInsetsCompat.Type.navigationBars())?.top ?: 0
+        val bottom = ViewCompat.getRootWindowInsets(activity.window.decorView)
+            ?.getInsetsIgnoringVisibility(WindowInsetsCompat.Type.navigationBars())?.bottom ?: 0
+        return abs(top - bottom)
+    }
+
+    /**
      * 获取状态栏高度
-     * TODO 是否包含状态栏和导航栏高度
      */
     @SuppressLint("InternalInsetResource")
     fun getStatusBarHeight(): Int {
@@ -103,7 +125,6 @@ object ScreenUtil {
 
     /**
      * 获取导航栏高度
-     * TODO 是否包含状态栏和导航栏高度
      */
     @SuppressLint("InternalInsetResource")
     fun getNavBarHeight(): Int {
@@ -150,9 +171,33 @@ object ScreenUtil {
     }
 
     /**
+     * 判断是否存在状态栏
+     */
+    fun isStatusBarVisible(activity: Activity): Boolean {
+        return ViewCompat.getRootWindowInsets(activity.window.decorView)
+            ?.isVisible(WindowInsetsCompat.Type.statusBars()) ?: true
+    }
+
+    /**
      * 隐藏状态栏
      */
     fun hideStatusBar(activity: Activity) {
+        ViewCompat.getWindowInsetsController(activity.window.decorView)
+            ?.hide(WindowInsetsCompat.Type.statusBars())
+    }
+
+    /**
+     * 显示状态栏
+     */
+    fun showStatusBar(activity: Activity) {
+        ViewCompat.getWindowInsetsController(activity.window.decorView)
+            ?.show(WindowInsetsCompat.Type.statusBars())
+    }
+
+    /**
+     * 隐藏状态栏，旧方式
+     */
+    fun hideStatusBarOld(activity: Activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             activity.window.insetsController?.hide(WindowInsets.Type.statusBars())
         } else {
@@ -161,14 +206,38 @@ object ScreenUtil {
     }
 
     /**
-     * 显示状态栏
+     * 显示状态栏，旧方式
      */
-    fun showStatusBar(activity: Activity) {
+    fun showStatusBarOld(activity: Activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             activity.window.insetsController?.show(WindowInsets.Type.statusBars())
         } else {
             activity.window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
         }
+    }
+
+    /**
+     * 导航栏是否可见
+     */
+    fun isNavBarVisible(activity: Activity): Boolean {
+        return ViewCompat.getRootWindowInsets(activity.window.decorView)
+            ?.isVisible(WindowInsetsCompat.Type.navigationBars()) ?: true
+    }
+
+    /**
+     * 隐藏导航栏
+     */
+    fun hideNavBar(activity: Activity) {
+        ViewCompat.getWindowInsetsController(activity.window.decorView)
+            ?.hide(WindowInsetsCompat.Type.navigationBars())
+    }
+
+    /**
+     * 显示导航栏
+     */
+    fun showNavBar(activity: Activity) {
+        ViewCompat.getWindowInsetsController(activity.window.decorView)
+            ?.show(WindowInsetsCompat.Type.navigationBars())
     }
 
 }
