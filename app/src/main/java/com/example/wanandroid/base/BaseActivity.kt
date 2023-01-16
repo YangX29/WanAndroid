f7c81fb2ec90d232cf3fc7446858615e59e9f664
YangX29 <1214719907@qq.com>
2023-01-16 21:49:44 +0800

    Activity基类添加沉浸式，主题修改

--- app/src/main/java/com/example/wanandroid/base/BaseActivity.kt
+++ app/src/main/java/com/example/wanandroid/base/BaseActivity.kt
@@ -1,7 +1,9 @@
 package com.example.wanandroid.base
 
+import android.os.Bundle
 import androidx.viewbinding.ViewBinding
 import com.example.module_common.base.BaseVBActivity
+import com.gyf.immersionbar.ImmersionBar
 
 /**
  * @author: Yang
@@ -10,5 +12,34 @@ import com.example.module_common.base.BaseVBActivity
  */
 abstract class BaseActivity<VB: ViewBinding> : BaseVBActivity<VB>(){
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //设置沉浸式
+        setImmersionBar()
+        //适配沉浸式状态栏
+        adaptImmersion()
+    }
+
+    /**
+     * 设置沉浸式状态栏
+     */
+    open fun setImmersionBar() {
+        ImmersionBar.with(this)
+            .statusBarDarkFont(statusBarDarkFont(), 0.2f)
+            .init()
+    }
+
+    /**
+     * 沉浸式适配
+     */
+    open fun adaptImmersion() {
+        //默认设置fitsSystemWindows
+        mBinding.root.fitsSystemWindows = true
+    }
+
+    /**
+     * 状态栏文字颜色，默认为深色模式
+     */
+    open fun statusBarDarkFont() = true
 
 }
