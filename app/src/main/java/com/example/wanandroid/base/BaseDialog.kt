a936339f5480ed9fb67900699a2718612904196e
YangX29 <1214719907@qq.com>
2023-01-31 21:17:34 +0800

    添加Dialog和DialogFragment基类

---
+++ app/src/main/java/com/example/wanandroid/base/BaseDialog.kt
@@ -0,0 +1,14 @@
+package com.example.wanandroid.base
+
+import android.content.Context
+import androidx.viewbinding.ViewBinding
+import com.example.module_common.base.BaseVBDialog
+
+/**
+ * @author: Yang
+ * @date: 2023/1/31
+ * @description: WanAndroid项目Dialog基类，继承[BaseVBDialog]
+ */
+class BaseDialog<VB : ViewBinding>(context: Context) : BaseVBDialog<VB>(context) {
+
+}
---
+++ app/src/main/java/com/example/wanandroid/base/BaseDialogFragment.kt
@@ -0,0 +1,13 @@
+package com.example.wanandroid.base
+
+import androidx.viewbinding.ViewBinding
+import com.example.module_common.base.BaseVBDialogFragment
+
+/**
+ * @author: Yang
+ * @date: 2023/1/31
+ * @description: WanAndroid项目Fragment基类，继承[BaseVBDialogFragment]
+ */
+class BaseDialogFragment<VB : ViewBinding> : BaseVBDialogFragment<VB>() {
+
+}
