a890127b1dae459432d7128271840195609e99c3
YangX29 <1214719907@qq.com>
2023-02-06 23:14:49 +0800

    module_common：添加LayoutParams扩展方法

--- module_common/src/main/java/com/example/module_common/utils/extension/ViewExtension.kt
+++ module_common/src/main/java/com/example/module_common/utils/extension/ViewExtension.kt
@@ -1,15 +1,24 @@
 package com.example.module_common.utils.extension
 
 import android.view.View
+import android.view.ViewGroup.LayoutParams
 
-fun View.visible(visible : Boolean = true) {
+fun View.visible(visible: Boolean = true) {
     visibility = if (visible) View.VISIBLE else View.INVISIBLE
 }
 
-fun View.invisible(invisible : Boolean = true) {
+fun View.invisible(invisible: Boolean = true) {
     visibility = if (!invisible) View.VISIBLE else View.INVISIBLE
 }
 
-fun View.gone(gone : Boolean = true) {
+fun View.gone(gone: Boolean = true) {
     visibility = if (gone) View.GONE else View.VISIBLE
+}
+
+inline fun <reified LP : LayoutParams> View.changeLayoutParams(changer: (LP) -> Unit) {
+    if (layoutParams is LP) {
+        val lp = layoutParams as LP
+        changer.invoke(lp)
+        layoutParams = lp
+    }
 }
