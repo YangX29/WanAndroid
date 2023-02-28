00beb3ee4498593bf2e81f55aa87d781d1c8de90
YangX29 <1214719907@qq.com>
2023-02-28 22:05:33 +0800

    修改Banner库

--- app/src/main/java/com/example/wanandroid/view/common/BannerHeaderView.kt
+++ app/src/main/java/com/example/wanandroid/view/common/BannerHeaderView.kt
@@ -4,17 +4,16 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
-import coil.load
 import com.alibaba.android.arouter.launcher.ARouter
 import com.example.wanandroid.R
 import com.example.wanandroid.common.RoutePath
 import com.example.wanandroid.databinding.HeaderHomeBannerBinding
+import com.example.wanandroid.databinding.ItemHomeBannerBinding
 import com.example.wanandroid.model.Banner
 import com.example.wanandroid.ui.web.WebActivity
 import com.example.wanandroid.utils.extension.loadWithDefault
-import com.youth.banner.adapter.BannerImageAdapter
-import com.youth.banner.holder.BannerImageHolder
-import com.youth.banner.indicator.CircleIndicator
+import com.zhpan.bannerview.BaseBannerAdapter
+import com.zhpan.bannerview.BaseViewHolder
 
 /**
  * @author: Yang
@@ -28,7 +27,7 @@ class BannerHeaderView @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     private val mBinding: HeaderHomeBannerBinding
-    private val mAdapter = BannerAdapter(listOf())
+    private val mAdapter = BannerAdapter()
 
     init {
         val root = View.inflate(context, R.layout.header_home_banner, this)
@@ -45,19 +44,12 @@ class BannerHeaderView @JvmOverloads constructor(
         mBinding.banner.apply {
             //adapter
             setAdapter(mAdapter)
-            //可滑动
-            setUserInputEnabled(true)
-            //指示器
-            indicator = CircleIndicator(context)
-            //指示器选中颜色
-            setIndicatorSelectedColorRes(R.color.banner_indicator_selected)
-            //指示器默认颜色
-            setIndicatorNormalColorRes(R.color.banner_indicator_unselected)
-            //自动轮播
-            isAutoLoop(true)
             //点击banner事件
-            setOnBannerListener { _, position -> clickBanner(position) }
+            setOnPageClickListener { _, position -> clickBanner(position) }
             //TODO 处理生命周期
+
+            //创建无数据banner
+            create()
         }
     }
 
@@ -65,7 +57,7 @@ class BannerHeaderView @JvmOverloads constructor(
      * 点击banner
      */
     private fun clickBanner(position: Int) {
-        val banner = mAdapter.getData(position)
+        val banner = mBinding.banner.data[position] as Banner
         //跳转到对应页面
         ARouter.getInstance().build(RoutePath.WEB)
             .withString(WebActivity.WEB_URL, banner.url)
@@ -73,31 +65,31 @@ class BannerHeaderView @JvmOverloads constructor(
     }
 
     /**
-     * banner点击事件
-     */
-    fun setOnBannerClick(callback: (Int) -> Unit) {
-
-    }
-
-    /**
      * 设置Banner数据
      */
     fun setBanner(banners: List<Banner>) {
-        mAdapter.setDatas(banners)
+        //TODO 设置缓存三个页面，修改重复加载图片问题
+        mBinding.banner.setOffScreenPageLimit(banners.size)
+        //加载数据
+        mBinding.banner.refreshData(banners)
     }
 
     /**
      * 轮播图Adapter
      */
-    private inner class BannerAdapter(data: List<Banner>) : BannerImageAdapter<Banner>(data) {
-
-        override fun onBindView(
-            holder: BannerImageHolder,
+    private inner class BannerAdapter : BaseBannerAdapter<Banner>() {
+        override fun bindData(
+            holder: BaseViewHolder<Banner>,
             data: Banner,
             position: Int,
-            size: Int
+            pageSize: Int
         ) {
-            holder.imageView?.loadWithDefault(data.imagePath)
+            val binding = ItemHomeBannerBinding.bind(holder.itemView)
+            binding.ivBanner.loadWithDefault(data.imagePath)
+        }
+
+        override fun getLayoutId(viewType: Int): Int {
+            return R.layout.item_home_banner
         }
 
     }
