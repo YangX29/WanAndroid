2d6bdd7e0d2c9d5e54be6cddb60095b84377cf60
YangX29 <1214719907@qq.com>
2023-08-02 20:50:36 +0800

    Resource扩展方法修改，解决无法获取黑夜模式色值问题

--- module_common/src/main/java/com/example/module_common/utils/extension/ResourceExt.kt
+++ module_common/src/main/java/com/example/module_common/utils/extension/ResourceExt.kt
@@ -1,5 +1,10 @@
 package com.example.module_common.utils.extension
 
+import android.annotation.SuppressLint
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.Drawable
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
@@ -13,26 +18,52 @@ import com.example.module_common.utils.app.ContextUtils
  */
 @ColorInt
 fun getColorRes(@ColorRes color: Int): Int {
-    return ContextUtils.getContext().getColor(color)
+    return getResource().getColor(color)
 }
 
 /**
  * 获取drawable
  */
+@SuppressLint("UseCompatLoadingForDrawables")
 fun getDrawableRes(@DrawableRes drawable: Int): Drawable? {
-    return ContextUtils.getContext().getDrawable(drawable)
+    return getResource().getDrawable(drawable)
+}
+
+/**
+ * 获取bitmap
+ */
+fun getBitmapRes(bitmap: Int): Bitmap {
+    return BitmapFactory.decodeResource(getResource(), bitmap)
 }
 
 /**
  * 获取String
  */
 fun getStringRes(@StringRes string: Int): String {
-    return ContextUtils.getContext().getString(string)
+    return getResource().getString(string)
 }
 
 /**
  * 获取dimension
  */
 fun getDimensionRes(@DimenRes dimension: Int): Float {
-    return ContextUtils.getContext().resources.getDimension(dimension)
+    return getResource().getDimension(dimension)
+}
+
+/**
+ * 获取resource
+ */
+fun getResource(): Resources {
+    val context = ContextUtils.getContext()
+    //当前模式
+    val currentNightMode =
+        context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+    //根据模式创建新的configuration和context
+    val configuration = Configuration(context.resources.configuration)
+    configuration.uiMode = if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+        Configuration.UI_MODE_NIGHT_YES
+    } else {
+        Configuration.UI_MODE_NIGHT_NO
+    }
+    return context.createConfigurationContext(configuration).resources
 }
