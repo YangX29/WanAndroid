be011620c8b1d4c2d09a45082dee0d36253d11de
YangX29 <39093842+YangX29@users.noreply.github.com>
2025-03-23 17:43:54 +0800

    feat:修改lifecycle生命周期扩展方法，doOnLife添加doOnlyOnce参数

--- module_common/src/main/java/com/example/module_common/utils/extension/LifecycleExt.kt
+++ module_common/src/main/java/com/example/module_common/utils/extension/LifecycleExt.kt
@@ -8,43 +8,55 @@ import androidx.lifecycle.LifecycleOwner
 /**
  * ON_CREATE回调
  */
-inline fun Lifecycle.doOnCreate(crossinline action: (LifecycleOwner) -> Unit) {
-    doOnLife(createAction = action)
+inline fun Lifecycle.doOnCreate(
+    crossinline action: (LifecycleOwner) -> Unit, doOnlyOnce: Boolean = true
+) {
+    doOnLife(createAction = action, doOnlyOnce = doOnlyOnce)
 }
 
 /**
  * ON_START回调
  */
-inline fun Lifecycle.doOnStart(crossinline action: (LifecycleOwner) -> Unit) {
-    doOnLife(startAction = action)
+inline fun Lifecycle.doOnStart(
+    crossinline action: (LifecycleOwner) -> Unit, doOnlyOnce: Boolean = true
+) {
+    doOnLife(startAction = action, doOnlyOnce = doOnlyOnce)
 }
 
 /**
  * ON_RESUME回调
  */
-inline fun Lifecycle.doOnResume(crossinline action: (LifecycleOwner) -> Unit) {
-    doOnLife(resumeAction = action)
+inline fun Lifecycle.doOnResume(
+    crossinline action: (LifecycleOwner) -> Unit, doOnlyOnce: Boolean = true
+) {
+    doOnLife(resumeAction = action, doOnlyOnce = doOnlyOnce)
 }
 
 /**
  * ON_PAUSE回调
  */
-inline fun Lifecycle.doOnPause(crossinline action: (LifecycleOwner) -> Unit) {
-    doOnLife(pauseAction = action)
+inline fun Lifecycle.doOnPause(
+    crossinline action: (LifecycleOwner) -> Unit, doOnlyOnce: Boolean = true
+) {
+    doOnLife(pauseAction = action, doOnlyOnce = doOnlyOnce)
 }
 
 /**
  * ON_STOP回调
  */
-inline fun Lifecycle.doOnStop(crossinline action: (LifecycleOwner) -> Unit) {
-    doOnLife(stopAction = action)
+inline fun Lifecycle.doOnStop(
+    crossinline action: (LifecycleOwner) -> Unit, doOnlyOnce: Boolean = true
+) {
+    doOnLife(stopAction = action, doOnlyOnce = doOnlyOnce)
 }
 
 /**
  * ON_DESTROY回调
  */
-inline fun Lifecycle.doOnDestroy(crossinline action: (LifecycleOwner) -> Unit) {
-    doOnLife(destroyAction = action)
+inline fun Lifecycle.doOnDestroy(
+    crossinline action: (LifecycleOwner) -> Unit, doOnlyOnce: Boolean = true
+) {
+    doOnLife(destroyAction = action, doOnlyOnce = doOnlyOnce)
 }
 
 /**
@@ -68,30 +80,49 @@ inline fun Lifecycle.doOnLife(
     crossinline pauseAction: ((LifecycleOwner) -> Unit) = {},
     crossinline stopAction: ((LifecycleOwner) -> Unit) = {},
     crossinline destroyAction: ((LifecycleOwner) -> Unit) = {},
+    doOnlyOnce: Boolean
 ) {
     addObserver(object : DefaultLifecycleObserver {
         override fun onCreate(owner: LifecycleOwner) {
             createAction.invoke(owner)
+            if (doOnlyOnce) {
+                removeObserver(this)
+            }
         }
 
         override fun onStart(owner: LifecycleOwner) {
             startAction.invoke(owner)
+            if (doOnlyOnce) {
+                removeObserver(this)
+            }
         }
 
         override fun onResume(owner: LifecycleOwner) {
             resumeAction.invoke(owner)
+            if (doOnlyOnce) {
+                removeObserver(this)
+            }
         }
 
         override fun onPause(owner: LifecycleOwner) {
             pauseAction.invoke(owner)
+            if (doOnlyOnce) {
+                removeObserver(this)
+            }
         }
 
         override fun onStop(owner: LifecycleOwner) {
             stopAction.invoke(owner)
+            if (doOnlyOnce) {
+                removeObserver(this)
+            }
         }
 
         override fun onDestroy(owner: LifecycleOwner) {
             destroyAction.invoke(owner)
+            if (doOnlyOnce) {
+                removeObserver(this)
+            }
         }
     })
 }
